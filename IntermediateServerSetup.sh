2f1db0acf0bb98a0516dec4d0b3c026d20de94b6
Alex De Young <90542032+ProfitBrd@users.noreply.github.com>
2023-12-14 00:06:22 -0600

    Update to reset firewall rules

--- IntermediateServerSetup.sh
+++ IntermediateServerSetup.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-# Script to set up SSH client and key-based authentication on Server A
+# Bash script to install necessary components, reset firewall rules, 
+# and allow all connections from a specific IP on Server B
 
-echo "Setting up SSH client on Server A..."
+echo "Starting setup..."
 
-# Update package list and install OpenSSH client
-sudo apt-get update
-sudo apt-get install -y openssh-client
+# Step 1: Install UFW
+echo "Installing UFW (Uncomplicated Firewall)..."
+sudo apt update
+sudo apt install ufw -y
+
+# Step 2: Install OpenSSH Server
+echo "Installing OpenSSH Server..."
+sudo apt install openssh-server -y
+
+# Step 3: Configure UFW
+echo "Configuring firewall..."
+
+# Reset UFW to default settings
+echo "Resetting UFW to default settings..."
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw --force reset
