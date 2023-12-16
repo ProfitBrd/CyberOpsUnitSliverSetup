a0d4ecfd4a7cde0716768417d2257270e358dcd5
Alex De Young <90542032+ProfitBrd@users.noreply.github.com>
2023-12-15 21:09:16 -0600

    Update w/ colors

--- ResetFirewallRules.sh
+++ ResetFirewallRules.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# ANSI Color Codes
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Resetting UFW (Uncomplicated Firewall) to default settings...${NC}"
+
 # Reset UFW to default settings
-echo "Resetting UFW to default settings..."
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw --force reset
+
+echo -e "${GREEN}UFW has been reset to its default state.${NC}"
