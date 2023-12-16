6b8dc54dcea2702b9beeedda9d7de1d789382f8e
Alex De Young <90542032+ProfitBrd@users.noreply.github.com>
2023-12-15 19:49:33 -0600

    Update IntermediateServerSetup.sh

--- IntermediateServerSetup.sh
+++ IntermediateServerSetup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Bash script to install necessary components, reset firewall rules, 
-# and allow all connections from a specific IP on Server B
+# and configure SSH on Server A (Intermediary Server)
 
 echo "Starting setup..."
 
-# Step 1: Install UFW
-echo "Installing UFW (Uncomplicated Firewall)..."
+# Step 1: Install UFW (Uncomplicated Firewall)
+echo "Installing UFW..."
 sudo apt update
 sudo apt install ufw -y
 
@@ -14,7 +14,19 @@ sudo apt install ufw -y
 echo "Installing OpenSSH Server..."
 sudo apt install openssh-server -y
 
-# Step 3: Configure UFW
+# Step 3: Check if SSH is running and start it if not
+echo "Checking if SSH is running..."
+ssh_status=$(systemctl is-active ssh)
+if [ "$ssh_status" != "active" ]; then
+    echo "SSH is not running. Starting SSH..."
+    sudo systemctl start ssh
+    sudo systemctl enable ssh
+    echo "SSH service started."
+else
+    echo "SSH is already running."
+fi
+
+# Step 4: Configure UFW
 echo "Configuring firewall..."
 
 # Reset UFW to default settings
@@ -22,3 +34,5 @@ echo "Resetting UFW to default settings..."
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw --force reset
+
+# Note: Further firewall configuration may be needed based on your specific requirements.
