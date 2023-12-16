6e866f329c6a77dceb2495d42fa50d2fc86ab29d
Alex De Young <90542032+ProfitBrd@users.noreply.github.com>
2023-12-15 20:59:52 -0600

    Add colors and current IP address

--- IntermediateServerSetup.sh
+++ IntermediateServerSetup.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
-# Bash script to install necessary components, reset firewall rules, 
-# and configure SSH on Server A (Intermediary Server)
+# ANSI Color Codes
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
 
-echo "Starting setup..."
+# Displaying the current IP address
+current_ip=$(hostname -I | cut -d' ' -f1)
+echo -e "${GREEN}Current IP Address of Server A: $current_ip${NC}"
+
+# Starting setup
+echo -e "${GREEN}Starting setup...${NC}"
 
 # Step 1: Install UFW (Uncomplicated Firewall)
-echo "Installing UFW..."
-sudo apt update
-sudo apt install ufw -y
+echo -e "${GREEN}Installing UFW (Uncomplicated Firewall)...${NC}"
+sudo apt-get update
+sudo apt-get install ufw -y
 
 # Step 2: Install OpenSSH Server
-echo "Installing OpenSSH Server..."
-sudo apt install openssh-server -y
+echo -e "${GREEN}Installing OpenSSH Server...${NC}"
+sudo apt-get install openssh-server -y
 
 # Step 3: Check if SSH is running and start it if not
-echo "Checking if SSH is running..."
+echo -e "${GREEN}Checking if SSH is running...${NC}"
 ssh_status=$(systemctl is-active ssh)
 if [ "$ssh_status" != "active" ]; then
-    echo "SSH is not running. Starting SSH..."
+    echo -e "${GREEN}SSH is not running. Starting SSH...${NC}"
     sudo systemctl start ssh
     sudo systemctl enable ssh
-    echo "SSH service started."
+    echo -e "${GREEN}SSH service started.${NC}"
 else
-    echo "SSH is already running."
+    echo -e "${GREEN}SSH is already running.${NC}"
 fi
 
 # Step 4: Configure UFW
-echo "Configuring firewall..."
+echo -e "${GREEN}Configuring firewall...${NC}"
 
 # Reset UFW to default settings
-echo "Resetting UFW to default settings..."
+echo -e "${GREEN}Resetting UFW to default settings...${NC}"
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw --force reset
 
-# Note: Further firewall configuration may be needed based on your specific requirements.
+echo -e "${GREEN}Firewall configuration complete. Further firewall configuration may be needed based on your specific requirements.${NC}"
