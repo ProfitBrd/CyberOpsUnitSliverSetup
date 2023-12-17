2ca0e9ddfe7317b442e8a516e536bc4fd58c5d12
Alex De Young <90542032+ProfitBrd@users.noreply.github.com>
2023-12-16 19:42:07 -0600

    Update cronJobForEscalatingPrivilages.sh

--- cronJobForEscalatingPrivilages.sh
+++ cronJobForEscalatingPrivilages.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Create root_cron_script.sh in the /tmp directory
-echo '#!/bin/bash' > /tmp/root_cron_script.sh
-echo 'echo "Current Time: $(date)"' >> /tmp/root_cron_script.sh
+SCRIPT_PATH="/tmp/root_cron_script.sh"
+LOG_FILE="/tmp/root_cron_log.log"
 
-# Make root_cron_script.sh executable and readable/writable by anyone
-chmod 777 /tmp/root_cron_script.sh
+# Create root_cron_script.sh in the /tmp directory
+echo '#!/bin/bash' > "$SCRIPT_PATH"
+echo "echo \"Current Time: \$(date)\" >> \"$LOG_FILE\"" >> "$SCRIPT_PATH"
+
+# Make root_cron_script.sh executable
+chmod +x "$SCRIPT_PATH"
 
 # Add a cron job entry for root to run root_cron_script.sh every minute
-(crontab -l 2>/dev/null; echo "* * * * * /tmp/root_cron_script.sh") | crontab -
+(sudo crontab -l 2>/dev/null; echo "* * * * * $SCRIPT_PATH") | sudo crontab -
