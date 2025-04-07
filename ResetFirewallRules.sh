412db237cc0bb5fe7d84afec5266bb6d2e0d56ab
Alex De Young <90542032+ProfitBrd@users.noreply.github.com>
2025-04-07 15:06:56 -0500

    remove redundant code

--- ResetFirewallRules.sh
+++ ResetFirewallRules.sh
@@ -9,8 +9,6 @@ NC='\033[0m' # No Color
 echo -e "${GREEN}Resetting UFW (Uncomplicated Firewall) to default settings...${NC}"
 
 # Reset UFW to default settings
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
 sudo ufw --force reset
 
 echo -e "${GREEN}UFW has been reset to its default state.${NC}"
